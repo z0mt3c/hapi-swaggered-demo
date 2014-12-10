4c17124e850eff778906a55456c10773d793da31
Timo Behrmann <timo.behrmann@gmail.com>
2014-12-10 20:45:14 +0100

    no message

--- example.js
+++ example.js
@@ -4,96 +4,93 @@ var hapiSwaggered = require('hapi-swaggered');
 var hapiSwaggeredUi = require('hapi-swaggered-ui');
 
 var server = new Hapi.Server();
-server.connection({ port: 8000, labels: ['api'], app: {
-		swagger: {
-			info: {
-				title: 'Example API2',
-				description: 'Tiny hapi-swaggered example2'
-			}
-		}
-} });
-
-server.register({
-	register: hapiSwaggered,
-	options: {
-		descriptions: {
-			'music': 'Example music description'
-		},
-		info: {
-			title: 'Example API',
-			description: 'Tiny hapi-swaggered example'
-		}
-	}
-}, {
-	select: 'api',
-	routes: {
-		prefix: '/swagger'
-	}
-}, function(err) {
-	if (err) {
-		throw err;
-	}
+server.connection({
+    port: 8000,
+    labels: ['api']
 });
 
 server.register({
-	register: hapiSwaggeredUi,
-	options: {
-		title: 'Example API',
-		authorization: {
-			field: 'apiKey',
-			scope: 'query' // header works as well
-			// valuePrefix: 'bearer '// prefix incase
-		}
-	}
+    register: hapiSwaggered,
+    options: {
+        descriptions: {
+            'foobar': 'Example foobar description'
+        },
+        info: {
+            title: 'Example API',
+            description: 'Tiny hapi-swaggered example'
+        }
+    }
 }, {
-	select: 'api',
-	routes: {
-		prefix: '/docs'
-	}
+    select: 'api',
+    routes: {
+        prefix: '/swagger'
+    }
 }, function(err) {
-	if (err) {
-		throw err;
-	}
+    if (err) {
+        throw err;
+    }
+});
+
+server.register({
+    register: hapiSwaggeredUi,
+    options: {
+        title: 'Example API',
+        authorization: {
+            field: 'apiKey',
+            scope: 'query' // header works as well
+            // valuePrefix: 'bearer '// prefix incase
+        }
+    }
+}, {
+    select: 'api',
+    routes: {
+        prefix: '/docs'
+    }
+}, function(err) {
+    if (err) {
+        throw err;
+    }
 });
 
 
 server.route({
-	path: '/',
-	method: 'GET',
-	handler: function(request, reply) {
-		reply.redirect('/docs');
-	}
+    path: '/',
+    method: 'GET',
+    handler: function(request, reply) {
+        reply.redirect('/docs');
+    }
 });
 
 server.route({
-	path: '/music/test',
-	method: 'GET',
-	config: {
-		tags: ['api'],
-		handler: function(request, reply) {
-			reply({});
-		}
-	}
+    path: '/foobar/test',
+    method: 'GET',
+    config: {
+        tags: ['api'],
+        description: 'My route description',
+        notes: 'My route notes',
+        handler: function(request, reply) {
+            reply({});
+        }
+    }
 });
 
-
 server.route({
-	path: '/music/{album}/{song}',
-	method: 'GET',
-	config: {
-		tags: ['api'],
-		validate: {
-			params: {
-				album: Joi.string().required().description('test'),
-				song: Joi.string().required()
-	}
-		},
-		handler: function(request, reply) {
-			reply({});
-		}
-	}
+    path: '/foobar/{foo}/{bar}',
+    method: 'GET',
+    config: {
+        tags: ['api'],
+        validate: {
+            params: {
+                foo: Joi.string().required().description('test'),
+                bar: Joi.string().required()
+            }
+        },
+        handler: function(request, reply) {
+            reply({});
+        }
+    }
 });
 
 server.start(function() {
-	console.log('started on http://localhost:8000');
+    console.log('started on http://localhost:8000');
 });
